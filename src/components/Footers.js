f0d3e62ee8435af9bcf445d8e7a4bc8cc007ec1d
sumonbarai <sumonbarai78.com>
2022-08-25 23:36:49 +0600

    footer section functionlity completed

--- src/components/Footers.js
+++ src/components/Footers.js
@@ -1,20 +1,80 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { colorChanged, statusChanged } from "../redux/filters/actionCreator";
+const numberOfTodos = (number) => {
+  switch (number) {
+    case 0:
+      return " No task";
+    case 1:
+      return " 1 task";
+
+    default:
+      return `${number} tasks`;
+  }
+};
 
 const Footers = () => {
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos);
+  const filters = useSelector((state) => state.filters);
+  const remainingTask = todos.filter((todo) => !todo.completed).length;
+  const { status, color } = filters;
+
+  const handleStatusChanged = (status) => {
+    dispatch(statusChanged(status));
+  };
+
+  const handleColorChanged = (newColor) => {
+    if (color.includes(newColor)) {
+      dispatch(colorChanged(newColor, "remove"));
+    } else {
+      dispatch(colorChanged(newColor, "added"));
+    }
+  };
   return (
     <div className="mt-4 flex justify-between text-xs text-gray-500">
-      <p>2 tasks left</p>
+      <p>{numberOfTodos(remainingTask)} left</p>
       <ul className="flex space-x-1 items-center text-xs">
-        <li className="cursor-pointer font-bold">All</li>
+        <li
+          className={`cursor-pointer ${status === "all" && "font-bold"}`}
+          onClick={() => handleStatusChanged("all")}
+        >
+          All
+        </li>
         <li>|</li>
-        <li className="cursor-pointer">Incomplete</li>
+        <li
+          className={`cursor-pointer ${status === "incomplete" && "font-bold"}`}
+          onClick={() => handleStatusChanged("incomplete")}
+        >
+          Incomplete
+        </li>
         <li>|</li>
-        <li className="cursor-pointer">Complete</li>
+        <li
+          className={`cursor-pointer ${status === "complete" && "font-bold"}`}
+          onClick={() => handleStatusChanged("complete")}
+        >
+          Complete
+        </li>
         <li></li>
         <li></li>
-        <li className="h-3 w-3 border-2 border-green-500 md:hover:bg-green-500 rounded-full cursor-pointer bg-green-500"></li>
-        <li className="h-3 w-3 border-2 border-red-500 md:hover:bg-red-500 rounded-full cursor-pointer"></li>
-        <li className="h-3 w-3 border-2 border-yellow-500 md:hover:bg-yellow-500 rounded-full cursor-pointer"></li>
+        <li
+          className={`h-3 w-3 border-2 border-green-500 md:hover:bg-green-500 rounded-full cursor-pointer ${
+            color.includes("green") && "bg-green-500"
+          }`}
+          onClick={() => handleColorChanged("green")}
+        ></li>
+        <li
+          className={`h-3 w-3 border-2 border-red-500 md:hover:bg-red-500 rounded-full cursor-pointer ${
+            color.includes("red") && "bg-red-500"
+          }`}
+          onClick={() => handleColorChanged("red")}
+        ></li>
+        <li
+          className={`h-3 w-3 border-2 border-yellow-500 md:hover:bg-yellow-500 rounded-full cursor-pointer ${
+            color.includes("yellow") && "bg-yellow-500"
+          }`}
+          onClick={() => handleColorChanged("yellow")}
+        ></li>
       </ul>
     </div>
   );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,12 +4,35 @@ import TodoItem from "./TodoItem";
 
 const TodoList = () => {
   const todos = useSelector((state) => state.todos);
+  const filters = useSelector((state) => state.filters);
+  const filterByStatus = (todo) => {
+    const { status } = filters;
+    switch (status) {
+      case "complete":
+        return todo.completed;
+      case "incomplete":
+        return !todo.completed;
+      default:
+        return true;
+    }
+  };
 
+  const filterByColor = (todo) => {
+    const { color } = filters;
+    if (color.length > 0) {
+      return color.includes(todo?.color);
+    } else {
+      return true;
+    }
+  };
   return (
     <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-      {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} />
-      ))}
+      {todos
+        .filter(filterByStatus)
+        .filter(filterByColor)
+        .map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
     </div>
   );
 };
